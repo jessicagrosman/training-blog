f4303000063ebb8d1bc02314c113c9167f634390
jessicagrosman <jess.grosman@gmail.com>
2019-03-31 19:40:15 -0400

    test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,15 +2,50 @@ var store = [{
         "title": "They just need to be aware of it",
         "excerpt":"I am always looking for ways to improve the eLearning courses I create. My main concern at the moment is creating solid courses that help my colleagues work more efficiently and effectively. This starts with goals. Good training isn’t about dumping information on learners, it’s about ensuring learners can use...","categories": ["reflections"],
         "tags": [],
-        "url": "/reflections/be-aware-of-it/",
+        "url": "http://localhost:4000/blog/reflections/be-aware-of-it/",
         "teaser":null},{
         "title": "No Evaulation? Possible side effects include",
         "excerpt":"How do you measure training effectiveness? Many projects are evaluated by two things: On Schedule? On Budget? These two elements completely ignore the point of training: people’s performance Many of us can agree that not measuring the results of training is bad, even if the best way to calculate ROI...","categories": ["reflections"],
         "tags": [],
-        "url": "/reflections/no-evaluation-side-effects/",
+        "url": "http://localhost:4000/blog/reflections/no-evaluation-side-effects/",
         "teaser":null},{
         "title": "Resume assistant: good JIT example",
         "excerpt":"I came across the Resume Assistant in Microsoft Word and LinkedIn. I thought it was a great example of Just-in-time training. Before we get into it, I want to talk about just-in-time training–it’s on my mind a lot lately. Oh and don’t worry the assistant is nothing like Clippy. The...","categories": ["examples"],
         "tags": [],
-        "url": "/examples/resume-assistant/",
+        "url": "http://localhost:4000/blog/examples/resume-assistant/",
+        "teaser":null},{
+        "title": "The Art of the Start in Documentation and Demos",
+        "excerpt":"Two mistakes about how to start software documentation and demos, dissected. Documentation I was checking a knowledgebase article this week. I skimmed the instructions. It all seemed straightforward, I thought. The instructions were detailed, step-by-step, with screenshots. Then I sat down, getting ready to follow the procedure. I saw the...","categories": ["reflections"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/reflections/start-doc-demo/",
+        "teaser":null},{
+        "title": "Storyline Challenge: MadLibs - Computer Security",
+        "excerpt":"Madlibs Challenge Over at E-Learning Heroes they had a challenge to use Madlibs in an elearning activity. I immediately had a flash when I saw the challenge. I could use it to make a funny little activity about leaving your computer unlocked at work. See the whole project here. With...","categories": ["storyline"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/storyline/challenge-madlibs-lock-pc/",
+        "teaser":null},{
+        "title": "Cyber Security Awareness Training: Passwords",
+        "excerpt":"I made a learning activity about choosing strong, complex passwords at work. It’s part of an E-learning Heroes’ challenge about web objects. I used the web objects to include activities that give feedback about password choices. I had a bit of trouble technically but learned new things about iframes. Read...","categories": ["storyline"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/storyline/challenge-passwords/",
+        "teaser":null},{
+        "title": "Cluttered UIs, Screen shots and Documentation",
+        "excerpt":"An experiement: 1) Take a screen shot of a cluttered UI; 2) do minimal edits; 3) make it easy for a user to quickly find the discussed feature When I take screen shots for documentation I often hum and haw over the User Interface. Am I showing too much? Or...","categories": ["documentation"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/documentation/cluttered-ui-screenshot-experiment/",
+        "teaser":null},{
+        "title": "Rebuilding Melissa Milloway's Coffee Cards",
+        "excerpt":"I have been rebuilding Melissa Milloway’s Educational Coffee Cards. It’s an Articulate Storyline project that presents information with a bit of flair. The coffee cards have nice little details, especially in the interactions, that give the project a polished user-friendly look. Emulate the greats At work I am the only...","categories": ["emulate-the-greats"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/emulate-the-greats/rebuilding-coffee-cards/",
+        "teaser":null},{
+        "title": "Leading Indicators as Customer Education Goals",
+        "excerpt":"I listen to podcasts when I commute. The morning commute is professional development time. I imagine podcast hosts and guests as dream colleagues–sharing tidbits and tips over the first coffee of the day. Customer Education Labs is a podcast by Adam Avramescu and Dave Derrington. They both work in Customer...","categories": ["notes"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/notes/customer-education-labs/",
+        "teaser":null},{
+        "title": "test",
+        "excerpt":"lalalalala  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/blog/test/",
         "teaser":null}]
